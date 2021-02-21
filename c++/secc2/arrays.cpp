6862ba3c3bbbeea4364693126130f14ce3157ef7
DarkCodeUV <leonardo.m2349@gmail.com>
2021-02-21 22:54:00 +0000

    Added the file brak and continue.

---
+++ c++/secc2/arrays.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	cout << "Array of intergers" << endl;
+	cout << "==================" << endl;
+	
+	int values[3];
+	values[0] = 88;
+	values[1] = 100;
+	values[2] = 1;
+
+
+	cout << values[0] << endl;
+	cout << values [1] << endl;
+	cout << values[2] << endl;
+
+	cout << "Array of double" << endl;
+	cout << "===============" << endl;
+	
+	double numbers[4] = {4.5, 2.3, 7.2, 8.1};
+
+	for(int i = 0; i < 4; i++){
+
+		cout << "Element of index " << i << numbers[i] << endl;
+	}
+	
+	return 0;
+}
