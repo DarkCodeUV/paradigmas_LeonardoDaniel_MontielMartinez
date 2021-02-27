ac092643854a2ace91836dd273f2d40208871720
DarkCodeUV <leonardo.m2349@gmail.com>
2021-02-26 19:38:18 -0600

    Added the file switch

---
+++ c++/secc2/switch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int value = 4;
+
+    switch (value){
+
+        case 4:
+            cout << "value is 4" << endl;
+        break;
+
+        case 5:
+            cout << "value is 5" << endl;
+        break;
+
+        case 6:
+            cout << "value is 6" << endl;
+        break;
+
+
+
+        default:
+            cout << "unrecognized value. " << endl;
+
+    }
+
+
+
+}
