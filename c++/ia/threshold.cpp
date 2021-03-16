c7ab2e951975a99d60e27f8ce44eb1d45a641f30
DarkCodeUV <leonardo.m2349@gmail.com>
2021-03-16 12:22:17 -0600

    add the file holpfield

--- c++/ia/threshold.cpp
+++ c++/ia/threshold.cpp
@@ -6,29 +6,38 @@ using namespace std;
 int H(double* w,double* x,int n){
 
     double sum = 0.0;
-    for(int i=0;i<=n;i++) { sum += w[i]*x[i]; }
+    for(int i=0;i<=n;i++) {
+        sum += w[i]*x[i]; 
+    }
     if(sum >= 0.0) return 1;
     else return 0;
 }
 
-int sign(double* w,double* x,int n){
+int sign(double* w,double* x,int n){    
     double sum = 0.0;
-    for(int i=0;i<=n;i++) { sum += w[i]*x[i];}
+    for(int i=0;i<=n;i++) { 
+        sum += w[i]*x[i];
+    }
     if(sum >= 0.0) return 1;
-    else return 0;
+    else if (sum == 0.0)return 0;
+    else if(sum < 0.0 ) return -1;
 }
 
 double unipolar(double* w,double* x,int n){
     double lambda = 1.0;
     double sum = 0.0;
-    for(int i=0;i<=n;i++) { sum += w[i]*x[i]; }
+    for(int i=0;i<=n;i++) { 
+        sum += w[i]*x[i]; 
+    }
     return 1.0/(1.0 + exp(-lambda*sum));
 }
 
 double bipolar(double* w,double* x,int n){
     double lambda = 1.0;
     double sum = 0.0;
-    for(int i=0;i<=n;i++) { sum += w[i]*x[i];}
+    for(int i=0;i<=n;i++) { 
+        sum += w[i]*x[i];
+    }
     return tanh(lambda*sum);
 }
 
